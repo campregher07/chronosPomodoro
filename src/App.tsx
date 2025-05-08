6999723c3f06dc2c1b186d1a56a83c62269ead56
campregher07 <danieljunioor38@gmail.com>
2025-05-08 19:01:56 -0300

    Aula 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-// PascalCase
-// PalavraOutraPalavra
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('oi');
   return (
     <>
-      <h1>Olá mundo</h1>
-      <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            tenetur, velit in molestias dolores magnam inventore, id explicabo,
+            molestiae sapiente nam eaque deleniti! Qui blanditiis nulla mollitia
+            ex earum voluptatum?
+          </section>
+        </div>
+      </div>
     </>
   );
 }
