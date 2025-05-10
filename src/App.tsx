ea2f49004b442792bd8fb18cd0d2a84711756360
campregher07 <danieljunioor38@gmail.com>
2025-05-10 12:04:07 -0300

    Aula 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './styles/theme.css';
 import './styles/global.css';
 
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            tenetur, velit in molestias dolores magnam inventore, id explicabo,
-            molestiae sapiente nam eaque deleniti! Qui blanditiis nulla mollitia
-            ex earum voluptatum?
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,13 @@
+import Styles from './styles.module.css';
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.content}>{children}</div>
+    </div>
+  );
+}
