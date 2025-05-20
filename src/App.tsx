bef06f4f3bb9d526cd5a5c4b08262277204fb5b1
campregher07 <danieljunioor38@gmail.com>
2025-05-20 17:43:23 -0300

    aula 33 + useState

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,30 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cylces } from './components/Cycles';
+import { DefaultButton } from './components/DeafualtButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
+import { useState } from 'react';
 
 export function App() {
+  const [num, setNum] = useState(0); // com o useState não pode ser usado atribuição diretamente
+  // const [num, setNum] = useState(() => {
+  //   return 0;
+  // }); // Usa essa Lazy inicialization qnd é necessario muitos calculos
+
+  function handleClick() {
+    setNum(prevState => prevState + 1); // regrinha de q qnd depende da variavel anterior, use essa func
+  }
+
   return (
     <>
+      <Heading>
+        <span id='numero'>Número: {num}</span>
+      </Heading>
+      <button onClick={handleClick}>Aumenta</button>
+
       <Container>
         <Logo />
       </Container>
@@ -28,7 +48,7 @@ export function App() {
             <DefaultInput
               id='meuInput'
               type='text'
-              labelTest='Task'
+              labelTest={num.toString()}
               placeholder='Digite Algo'
             />
           </div>
@@ -38,14 +58,17 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 </p>
+            <Cylces />
           </div>
 
           <div className='formRow'>
-            <button>enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
