0c09b337895fde11154186295060123408c6ecf9
campregher07 <danieljunioor38@gmail.com>
2025-05-21 11:00:25 -0300

    aula 37

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,9 @@ import { Cylces } from './components/Cycles';
 import { DefaultButton } from './components/DeafualtButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
-
 export function App() {
-  const [num, setNum] = useState(0); // com o useState não pode ser usado atribuição diretamente
-  // const [num, setNum] = useState(() => {
-  //   return 0;
-  // }); // Usa essa Lazy inicialization qnd é necessario muitos calculos
-
-  function handleClick() {
-    setNum(prevState => prevState + 1); // regrinha de q qnd depende da variavel anterior, use essa func
-  }
-
   return (
     <>
-      <Heading>
-        <span id='numero'>Número: {num}</span>
-      </Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo />
       </Container>
@@ -48,7 +31,7 @@ export function App() {
             <DefaultInput
               id='meuInput'
               type='text'
-              labelTest={num.toString()}
+              labelTest='task'
               placeholder='Digite Algo'
             />
           </div>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,75 @@
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
-import { HistoryIcon, SettingsIcon, SunIcon, TimerIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+
+type AvaliableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvaliableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); //não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+    // document.documentElement.setAttribute('data-theme', theme); // efeito colateral é algo que o React nao esta monitorando
+  }
+
+  // useEffect(() => {
+  //   console.log('useEffect sem dependencia', Date.now());
+  // }); // executa td vez q o componente renderiza na tela
+
+  // useEffect(() => {
+  //   console.log('useEffect com deps vazias', Date.now());
+  // }, []); // executa apenas qnd o react monta o compennete na tela pela primeira vez
+
+  useEffect(() => {
+    console.log('theme:', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('O componente sera atualizado');
+    };
+  }, [theme]); // executa apenas qnd o valor de theme muda
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
-        <TimerIcon />
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a home'
+        title='Ir para a home'
+      >
+        <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para configurações'
+        title='Ir para configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
