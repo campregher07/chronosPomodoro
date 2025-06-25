54a78081ead3b1056be2133373ee046750f82ee8
campregher07 <danieljunioor38@gmail.com>
2025-06-25 15:25:20 -0300

    aula 40

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,10 @@
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
-import { Menu } from './components/Menu';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cylces } from './components/Cycles';
-import { DefaultButton } from './components/DeafualtButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              type='text'
-              labelTest='task'
-              placeholder='Digite Algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cylces />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cylces } from '../Cycles';
+import { DefaultButton } from '../DeafualtButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          id='meuInput'
+          type='text'
+          labelTest='task'
+          placeholder='Digite Algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cylces />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} color='green' />
+      </div>
+    </form>
+  );
+}
